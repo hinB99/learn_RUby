e2e8bba3e6e22762f20d8f4310444e2bbfb02a3d
hinB99 <102239973+hinB99@users.noreply.github.com>
2022-07-01 20:43:53 +0700

    m

---
+++ bai_255.rb
@@ -0,0 +1,16 @@
+#Sắp xếp mảng tăng dần
+def sap_xep(ar)
+    sort = false
+    while !sort
+        sort = true
+        (0...ar.length - 1).each do |value|
+            if ar[value] > ar[value + 1]
+                ar[value], ar[value + 1] = ar[value + 1], ar[value]
+                sort = false
+            end
+        end
+    end
+    return ar
+end
+
+puts sap_xep([1, 2, 7, 5, 4, 9])
